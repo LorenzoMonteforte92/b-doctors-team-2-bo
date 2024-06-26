b303d3b68e6153bc9a6b6212fb496677b4aaecd9
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-06-26 17:08:36 +0200

    feat: add foreing key in profiles table

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -45,9 +45,9 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show()
+    public function show(User $user)
     {
-        
+        return view('admin.profiles.show', compact('user'));
     }
 
     /**
