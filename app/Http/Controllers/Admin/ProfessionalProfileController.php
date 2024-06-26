a9d1648841d5963fa6f6f03d9797658cce0d346b
codewithFRANKGIGLI <gigli.francesco099@gmail.com>
2024-06-26 17:11:22 +0200

    validation rule in controller

--- app/Http/Controllers/Admin/ProfessionalProfileController.php
+++ app/Http/Controllers/Admin/ProfessionalProfileController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
+use Illuminate\Validation\Rule;
 use App\Models\Review;
 use App\Models\Rating;
 use App\Models\Message;
