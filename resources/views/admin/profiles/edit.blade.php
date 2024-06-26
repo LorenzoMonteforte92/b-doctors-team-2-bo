254bc6b5c5ba27a5b3d065227e3b2e68699f0d98
CiroMarra <tensha93@gmail.com>
2024-06-26 14:49:30 +0200

    Fix Edit and route for edit

--- resources/views/admin/profiles/edit.blade.php
+++ resources/views/admin/profiles/edit.blade.php
@@ -3,42 +3,41 @@
 @section('content')
 <div class="container">
     <h2>Edita Profilo</h2>
-    <form action="{{ route('admin.profiles.update', $profile->slug) }}" method="POST" enctype="multipart/form-data">
+    <form action="{{ route('admin.profiles.update', $profile->id) }}" method="POST" enctype="multipart/form-data">
         @csrf
         @method('PUT')
 
-
         <div class="form-group">
             <label for="photo">Foto Profilo:</label>
-            <input type="text" class="form-control" id="photo" name="photo" value="{{ $profiles->photo }}">
+            <input type="text" class="form-control" id="photo" name="photo" value="{{ old('photo', $profile->photo) }}">
         </div>
 
         <div class="form-group">
             <label for="telephone_number">Numero di telefono:</label>
-            <input type="text" class="form-control" id="telephone_number" name="telephone_number" value="{{ $profiles->telephone_number }}">
+            <input type="text" class="form-control" id="telephone_number" name="telephone_number" value="{{ old('telephone_number', $profile->telephone_number) }}">
         </div>
 
         <div class="form-group">
             <label for="curriculum_vitae">Curriculum Vitae:</label>
-            <textarea class="form-control" id="curriculum_vitae" name="curriculum_vitae">{{ $profiles->curriculum_vitae }}</textarea>
+            <textarea class="form-control" id="curriculum_vitae" name="curriculum_vitae">{{ old('curriculum_vitae', $profile->curriculum_vitae) }}</textarea>
         </div>
 
         <div class="form-group">
             <label for="bio">Bio:</label>
-            <textarea class="form-control" id="bio" name="bio">{{ $profiles->bio }}</textarea>
+            <textarea class="form-control" id="bio" name="bio">{{ old('bio', $profile->bio) }}</textarea>
         </div>
 
         <div class="form-group">
             <label for="performance">Prestazioni:</label>
-            <textarea class="form-control" id="performance" name="performance">{{ $profiles->performance }}</textarea>
+            <textarea class="form-control" id="performance" name="performance">{{ old('performance', $profile->performance) }}</textarea>
         </div>
 
         <div class="form-group">
-            <label for="visibility">Visibility:</label>
-            <input type="checkbox" id="visibility" name="visibility" {{ $profiles->visibility ? 'checked' : '' }}>
+            <label for="visibility">Visibilità:</label>
+            <input type="checkbox" id="visibility" name="visibility" {{ old('visibility', $profile->visibility) ? 'checked' : '' }}>
         </div>
 
-        <button type="submit" class="btn btn-primary">Update</button>
+        <button type="submit" class="btn btn-primary">Aggiorna</button>
     </form>
 </div>
 @endsection
