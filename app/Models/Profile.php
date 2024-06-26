649143e84ea8bf2e325558ba2d9ad38c218ee410
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-06-26 13:00:48 +0200

    feat: add profile rating and review relations

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Review;
+use App\Models\User;
 
 class Profile extends Model
 {
     use HasFactory;
+
+    public function reviews() {
+        return $this->hasMany(Review::class);
+     }
+
+     public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Profile;
+use App\Models\Rating;
 
 class Review extends Model
 {
     use HasFactory;
+
+    public function profile() {
+        return $this->belongsTo(Profile::class);
+    }
+
+    public function ratings() {
+        return $this->hasMany(Rating::class);
+     }
 }
