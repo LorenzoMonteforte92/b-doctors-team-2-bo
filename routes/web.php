ec33961e9bc4d1f432b50564e308a879984a581a
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-06-28 11:04:08 +0200

    fix

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\ProfessionalProfileController;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
