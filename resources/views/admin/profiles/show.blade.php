ff5a9deb24b2fda06adb9e046fddcc67b299e5c1
CiroMarra <tensha93@gmail.com>
2024-06-28 12:26:10 +0200

    Fix: add some fix

--- resources/views/admin/profiles/show.blade.php
+++ resources/views/admin/profiles/show.blade.php
@@ -13,11 +13,11 @@
         @if ($profile->photo)
             <img src="{{ asset('storage/' . $profile->photo) }}" alt="{{ $profile->name }}">
         @endif
-        <div class="my-2 brand-text-color-1 "><strong>ID</strong>: {{ $profile->id }}</div>
-        <div class="my-2 brand-text-color-1 "><strong>EMAIL</strong>: {{ $user->email }}</div>
-        <div class="my-2 brand-text-color-1 "><strong>NOME</strong>: {{ $user->name }}</div>
-        <div class="my-2 brand-text-color-1 "><strong>INDIRIZZO</strong>: {{ $user->address }}</div>
-        <div class="my-2 brand-text-color-1 "><strong>TELEFONO</strong>: {{ $profile->telephone_number }}</div>
+        <div class="my-2"><strong class="brand-text-color-1 ">ID</strong>: {{ $profile->id }}</div>
+        <div class="my-2"><strong class="brand-text-color-1 ">EMAIL</strong>: {{ $user->email }}</div>
+        <div class="my-2"><strong class="brand-text-color-1 ">NOME</strong>: {{ $user->name }}</div>
+        <div class="my-2 "><strong class="brand-text-color-1 ">INDIRIZZO</strong>: {{ $user->address }}</div>
+        <div class="my-2 "><strong class="brand-text-color-1 ">TELEFONO</strong>: {{ $profile->telephone_number }}</div>
         @if ($profile->curriculum_vitae)
             <div><strong class="brand-text-color-1 ">CURRICULUM:</strong></div>
             <img src="{{ asset('storage/' . $profile->curriculum_vitae) }}" alt="{{ $profile->name }}">
