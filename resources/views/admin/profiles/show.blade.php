2efd6506b3ffa68e704433805b285ee75554b18d
CiroMarra <tensha93@gmail.com>
2024-07-01 14:51:11 +0200

    feat: add img fluid to img photo and curriculum

--- resources/views/admin/profiles/show.blade.php
+++ resources/views/admin/profiles/show.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.admin')
 
 @section('content')
+    
         <h2 class="brand-text-color-1">Il tuo profilo</h2>
 
         @if (session()->has('message'))
@@ -10,7 +11,7 @@
         @endif
         <div class="profile-wrapper">
             @if ($profile->photo)
-                <img src="{{ asset('storage/' . $profile->photo) }}" alt="{{ $profile->name }}">
+                <img  class="img-fluid" src="{{ asset('storage/' . $profile->photo) }}" alt="{{ $profile->name }}">
             @endif
             <div class="my-2"><strong class="brand-text-color-1 ">Email:</strong> {{ $user->email }}</div>
             <div class="my-2"><strong class="brand-text-color-1 ">Nome:</strong> {{ $user->name }}</div>
@@ -18,7 +19,7 @@
             <div class="my-2 "><strong class="brand-text-color-1 ">Numero di Telefono:</strong> {{ $profile->telephone_number }}</div>
             @if ($profile->curriculum_vitae)
                 <div><strong class="brand-text-color-1 ">Curriculum Vitae</strong></div>
-                <img src="{{ asset('storage/' . $profile->curriculum_vitae) }}" alt="{{ $profile->name }}">
+                <img  class="img-fluid" src="{{ asset('storage/' . $profile->curriculum_vitae) }}" alt="{{ $profile->name }}">
             @endif
             <div><strong class="my-2 brand-text-color-1">Specializzazioni:</strong>
                 @if (count($profile->specialisations) > 0)
@@ -28,6 +29,7 @@
                 @endif
             </div>
             <div class="my-2"><strong class="brand-text-color-1">Prestazioni:</strong> {{ $profile->performance }}</div>
+
             <div class="my-2"><strong class="brand-text-color-1">Biografia:</strong> {{ $profile->bio }}</div>
-        </div>
+        </div>   
 @endsection
