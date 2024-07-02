bdef51acc52fb83f1c63c3c4ebbcdf76a0b96eb8
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-07-02 17:56:23 +0200

    feat: add join reviews and ratings to /specialisations/slug API

--- app/Http/Controllers/Api/ProfessionalProfileController.php
+++ app/Http/Controllers/Api/ProfessionalProfileController.php
@@ -43,9 +43,11 @@ class ProfessionalProfileController extends Controller
 
         $profile = DB::table('profile_specialisation')
                    ->join('profiles', 'profile_specialisation.profile_id','=', 'profiles.id')
+                   ->join('reviews', 'reviews.profile_id', '=', 'profiles.id' )
+                   ->join('ratings', 'reviews.profile_id', '=', 'ratings.id' )
                    ->join('specialisations', 'profile_specialisation.specialisation_id', '=', 'specialisations.id')
                    ->join('users', 'profiles.user_id' , '=', 'users.id', )
-                   ->select('specialisations.name AS spec_name', 'specialisations.slug AS spec_slug', 'profile_specialisation.*', 'users.name AS user_name', 'users.slug AS user_slug', 'users.email AS user_mail', 'profiles.*' )
+                   ->select('specialisations.name AS spec_name','specialisations.slug AS spec_slug', 'profile_specialisation.*', 'users.name AS user_name', 'users.slug AS user_slug', 'users.email AS user_mail', 'profiles.*','reviews.*', 'ratings.*')
                    ->where('specialisations.slug', '=', $slug)
                    ->get();
 
