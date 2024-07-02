5698a5ef41d86f15c9e1284b78bc1cc16bb8c979
codewithFRANKGIGLI <gigli.francesco099@gmail.com>
2024-07-02 15:39:03 +0200

    feat: rating controller

--- app/Http/Controllers/Api/RatingController.php
+++ app/Http/Controllers/Api/RatingController.php
@@ -5,12 +5,39 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Rating;
+use Illuminate\Support\Facades\Validator;
+
 
 class RatingController extends Controller
 {
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         $data = $request->all();
 
-        dd($data);
+        $validator = Validator::make(
+            $data,
+            [
+                'score' => 'required|numeric',
+            ],
+            [
+                'score.required' => 'Devi selezionare un voto da 1 a 5.',
+            ]
+        );
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors(),
+            ]);
+        }
+        $rating = new Rating();
+        $rating->fill($data);
+        $rating->save();
+
+
+        // return
+        return response()->json([
+            'success' => true,
+            'message' => 'Voto salvato correttamente.',
+        ]);
     }
 }
