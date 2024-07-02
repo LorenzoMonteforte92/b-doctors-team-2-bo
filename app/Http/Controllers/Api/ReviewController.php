d5ef8a68bac0e7c155dade5812a22d29c7bb42dd
codewithFRANKGIGLI <gigli.francesco099@gmail.com>
2024-07-02 15:25:52 +0200

    feat: review controller

--- app/Http/Controllers/Api/ReviewController.php
+++ app/Http/Controllers/Api/ReviewController.php
@@ -4,16 +4,40 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Review;
+use Illuminate\Support\Facades\Validator;
+
 
 class ReviewController extends Controller
 {
     public function store(Request $request){
         $data = $request->all();
 
-       dd($data);
+        // validazione name e description
+        $validator = Validator::make($data, [
+            'name' => 'required|string|max:255',
+            'description' => 'required|string|max:255',
+        ],
+        [
+            'name.required' => 'Il campo nome è obbligatorio',
+            'description.required' => 'Il campo descrizione è obbligatorio',
+        ]);
 
-        // return response()->json([
-        //     'success' => true
-        // ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'errors' => $validator->errors()
+            ]);
+        }
+        // salvo i dati nel DB
+        $review = new Review();
+        $review->fill($data);
+        $review->save();
+        
+        
+        return response()->json([
+            'message' => 'Review created successfully',
+            'data' => $review], 201);
+
     }
 }
